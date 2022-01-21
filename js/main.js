1ecade5cce6b63e05bc1b80a8c5d577ca3189c9f
MENG.MAO <meng.mao@PNC.LAN>
2022-01-21 18:17:18 +0700

    check for mistkes

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let containerHome = document.querySelector('.container-home');
+// To make fake list of questions
+let listOfQuestions = document.createElement('div');
+listOfQuestions.className = "edit-quiz";
+containerHome.appendChild(listOfQuestions);
+console.log(containerHome);
