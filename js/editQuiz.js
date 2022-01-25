444541a354c8aa3d843af87c447eb7d8e5303836
MENG.MAO <MENG.MAO@PNC.LAN>
2022-01-25 17:37:39 +0700

    add tick sign

--- js/editQuiz.js
+++ js/editQuiz.js
@@ -1,199 +1,199 @@
-let datas = [{
+let listOfQuestions = [{
     question: "Q-1: How many provinces in Cambodia?Capital city?",
-    answer: [{
-            a: " A: 24 Phnom Penh",
+    listOfAnswers: [{
+            answer: " A: 24 Phnom Penh",
             isCorrect: true,
         },
         {
-            a: " B: 25 Kampong Thum",
+            answer: " B: 25 Kampong Thum",
             isCorrect: false,
         },
         {
-            a: " C: 24 Kandal",
+            answer: " C: 24 Kandal",
             isCorrect: false,
         },
         {
-            a: " D: 25 Siem Reap",
+            answer: " D: 25 Siem Reap",
             isCorrect: false,
         }
     ]
 },
 {
     question: "Q-2: what is the most famous temple in Thailand?",
-    answer: [{
-            a: " A: Wat Pho",
+    listOfAnswers: [{
+        answer: " A: Wat Pho",
             isCorrect: false,
         },
         {
-            a: " B: Wat Arun",
+            answer: " B: Wat Arun",
             isCorrect: false,
         },
         {
-            a: " C: White temple",
+            answer: " C: White temple",
             isCorrect: true,
         },
         {
-            a: " D: Blue temple",
+            answer: " D: Blue temple",
             isCorrect: false,
         }
     ]
 },
 {
     question: "Q-3: what is the independence's day of Vietnam?",
-    answer: [{
-            a: " A: 1940",
+    listOfAnswers: [{
+        answer: " A: 1940",
             isCorrect: false,
         },
         {
-            a: " B: 1845",
+            answer: " B: 1845",
             isCorrect: false,
         },
         {
-            a: " C: 1945",
+            answer: " C: 1945",
             isCorrect: true,
         },
         {
-            a: " D: 1955",
+            answer: " D: 1955",
             isCorrect: false,
         }
     ]
 },
 {
     question: "Q-4: How much arable land of Lao?",
-    answer: [{
-            a: " A: 236800",
+    listOfAnswers: [{
+        answer: " A: 236800",
             isCorrect: true,
         },
         {
-            a: " B: 330179",
+            answer: " B: 330179",
             isCorrect: false,
         },
         {
-            a: " C: 235800",
+            answer: " C: 235800",
             isCorrect: false,
         },
         {
-            a: " D: 210000",
+            answer: " D: 210000",
             isCorrect: false,
         }
     ]
 },
 {
     question: "Q-5: when did Myanmar joined ASEAN?",
-    answer: [{
-            a: " A: 23/6/1997",
+    listOfAnswers: [{
+        answer: " A: 23/6/1997",
             isCorrect: false,
         },
         {
-            a: " B: 23/7/1997",
+            answer: " B: 23/7/1997",
             isCorrect: true,
         },
         {
-            a: " C: 22/7/1997",
+            answer: " C: 22/7/1997",
             isCorrect: false,
         },
         {
-            a: " D: 23/8/1997",
+            answer: " D: 23/8/1997",
             isCorrect: false,
         }
     ]
 },
 {
-    question: "Q-6: How many seasons in Sinaapore?",
-    answer: [{
-            a: " A: 2",
+    question: "Q-6: How many seasons in Singapore?",
+    listOfAnswers: [{
+        answer: " A: 2",
             isCorrect: true,
         },
         {
-            a: " B: 4",
+            answer: " B: 4",
             isCorrect: false,
         },
         {
-            a: " C: 3",
+            answer: " C: 3",
             isCorrect: false,
         },
         {
-            a: " D: 1",
+            answer: " D: 1",
             isCorrect: false,
         }
     ]
 },
 {
     question: "Q-7: What country did Malaysia aet independence from?",
-    answer: [{
-            a: " A: France",
+    listOfAnswers: [{
+        answer: " A: France",
             isCorrect: false,
         },
         {
-            a: " B: USA",
+            answer: " B: USA",
             isCorrect: false,
         },
         {
-            a: " C: Span",
+            answer: " C: Span",
             isCorrect: false,
         },
         {
-            a: " D: Enalish",
+            answer: " D: Enalish",
             isCorrect: true,
         }
     ]
 },
 {
     question: "Q-8: what is the laraest and capital city of Indonesia?",
-    answer: [{
-            a: " A: Jakata",
+    listOfAnswers: [{
+        answer: " A: Jakata",
             isCorrect: true,
         },
         {
-            a: " B: Surabaya",
+            answer: " B: Surabaya",
             isCorrect: false,
         },
         {
-            a: " C: Makasar",
+            answer: " C: Makasar",
             isCorrect: false,
         },
         {
-            a: " D: Banduna",
+            answer: " D: Banduna",
             isCorrect: false,
         }
     ]
 },
 {
     question: "Q-9: What is the reliaion of Brunei?",
-    answer: [{
-            a: " A: Buddhist",
+    listOfAnswers: [{
+        answer: " A: Buddhist",
             isCorrect: false,
         },
         {
-            a: " B: Christain",
+            answer: " B: Christain",
             isCorrect: false,
         },
         {
-            a: " C: Chatolic",
+            answer: " C: Chatolic",
             isCorrect: false,
         },
         {
-            a: " D: Sunni Islam",
+            answer: " D: Sunni Islam",
             isCorrect: true,
         }
     ]
 },
 {
     question: "Q-10: How many island in Philippines?",
-    answer: [{
-            a: " A: 7100",
+    listOfAnswers: [{
+        answer: " A: 7100",
             isCorrect: false,
         },
         {
-            a: " B: 6900",
+            answer: " B: 6900",
             isCorrect: false,
         },
         {
-            a: " C: 7000",
+            answer: " C: 7000",
             isCorrect: true,
         },
         {
-            a: " D: 6800",
+            answer: " D: 6800",
             isCorrect: false,
         }
     ]
@@ -201,41 +201,47 @@ let datas = [{
 ];
 
 
-//   To get datas from local storages
-localStorage.setItem('quiz', JSON.stringify(datas));
+//   =============>>>>>>>>> To get datas from local storages <<<<<<<==========================
+localStorage.setItem('quiz', JSON.stringify(listOfQuestions));
 let newData = JSON.parse(localStorage.getItem('quiz'));
 
 let body = document.querySelector('body');
-//    To create edit container 
+//    ============>>>>>>>>>>>To create edit container <<<<<<<<<<<<=============================
 let editCon = document.createElement('div');
 editCon.className = "edit-con";
 body.appendChild(editCon);
 
-let form = document.createElement('form');
+const form = document.createElement('form');
 form.className = "form";
 editCon.appendChild(form);
-console.log(form);
+
 // ============================ loop to create list to store data o DOM =======================
-for (let el of datas) {
-    let quest = document.createElement('h3');
-    quest.className = "question";
-    quest.textContent = el['question'];
-    form.appendChild(quest);
-    for (let values of el['answer']) {
+for (let questions of listOfQuestions) {
+    let question = document.createElement('h3');
+    question.className = "question";
+    question.textContent = questions['question'];
+    form.appendChild(question);
+    for (let values of questions['listOfAnswers']) {
         let ol = document.createElement('ol');
-        ol.className = 'ol';
+        ol.className = 'ol-edit';
         let radio = document.createElement('input');
         radio.type = 'radio';
+        radio.name = "answer";
         radio.className = "radio";
         let li = document.createElement('li');
-        li.textContent = values['a'];
+        li.className = "li-edit";
+        li.textContent = values['answer'];
         ol.appendChild(radio);
         ol.appendChild(li);
         form.appendChild(ol);
-        // To know which one is correct
-        if(values['isCorrect']==true){
-            li.textContent +=" ==> True";
-            ol.style.backgroundColor = "yellowgreen";
+// =========>>>>>>>> To know which one is correct  <<<<<<<========================
+        if(values['isCorrect']===true){
+            let tickImg = document.createElement('img');
+            tickImg.className = "tickImg";
+            tickImg.src = "/image/tick.png";
+            ol.appendChild(tickImg);
+            ol.style.display = "/image/tick.png";
+            console.log(ol);
         }
 
     }
